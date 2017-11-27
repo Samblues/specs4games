5d91c6e64e2944b9b6a68d5523fa10eb84fce1ab
Sam Guzel <taw.samblues@gmail.com>
2017-11-27 18:39:07 +0000

    Pages and look

--- js/mobilefriendly.js
+++ js/mobilefriendly.js
@@ -1,3 +1,11 @@
 $('.tap-target').tapTarget('open');
 $('.tap-target').tapTarget('close');
 
+$(document).ready(function(){
+    $('.carousel').carousel();
+});
+
+$('.carousel.carousel-slider').carousel({fullWidth: true});
+$(document).ready(function() {
+    $('select').material_select();
+  });
